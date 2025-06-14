ba39e7170edd7426ab53ce352c5a0f14fd6e5a23
Abuzar Ghafari <abuzarr.ghafari@gmail.com>
2025-06-14 20:48:07 +0500

    view returned form web route

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware('web')->get('/theme-test', function () {
+    return view('theme-test');
     dd("light-theme");
     return Inertia::render('ThemeTest', ['theme' => 'light']);
 });
