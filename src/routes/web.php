64e49e87e41f3b6bd29e92c8a2d0f797affe0168
Abuzar Ghafari <abuzarr.ghafari@gmail.com>
2025-06-14 20:37:07 +0500

    dd light theme

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::middleware('web')->get('/theme-test', function () {
+    dd("light-theme");
     return Inertia::render('ThemeTest', ['theme' => 'light']);
 });
